8a52012b097012f01932426bda05dbf6f1818b02
crolek <c.rolek@gmail.com>
2013-09-04 23:24:38 -0500

    Adding Example4 initial commit. It still needs some work on the README file.

---
+++ Example4/Gruntfile.js
@@ -0,0 +1,46 @@
+module.exports = function(grunt) {
+	grunt.loadNpmTasks("grunt-contrib-less");
+	grunt.loadNpmTasks("grunt-contrib-concat");
+	grunt.loadNpmTasks("grunt-contrib-uglify");
+	grunt.loadNpmTasks("grunt-contrib-watch");
+
+	// Project configuration.
+	grunt.initConfig({
+		concat: {
+			prod: {
+				src: ["src/js/*.js"],
+				dest: "dist/js/all-js.js"
+			}
+		},
+		less: {
+			allFiles: {
+				files: [
+					{"dist/css/common.css": "src/less/*.less"}
+				]
+			}
+		},
+		uglify: {
+			js: {
+				src: "dist/js/all-js.js",
+				dest: "dist/js/all-js.min.js"
+			}
+		},
+		watch: {
+			onlyWatchingTheHtmlFile: {
+				files: ["src/test.html"],
+				tasks: ["concat", "less"]
+			},
+			dev: {
+				files: ["src/js/*.js", "src/less/*.less"],
+				tasks: ["concat:prod", "less"]
+			},
+			prod: {
+				files: ["src/js/*.js", "src/less/*.less"],
+				tasks: ["concat:prod", "less", "uglify"]
+			}
+		}
+	});
+
+	grunt.registerTask("dev", ["concat", "less"]);
+	grunt.registerTask("prod", ["concat", "less", "uglify"]);
+};
